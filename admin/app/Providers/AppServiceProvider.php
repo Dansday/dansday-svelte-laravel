e4f72293a7148a7836ee21adc71bc8092fbedfbd
Dansday <akbaryudhanto49@gmail.com>
2026-02-22 19:00:53 +0800

    Add automatic eager loading for Eloquent models in AppServiceProvider. This enhancement improves performance by reducing the number of queries executed when accessing related models.

--- admin/app/Providers/AppServiceProvider.php
+++ admin/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
@@ -16,6 +17,8 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Model::automaticallyEagerLoadRelationships();
+
         Schema::defaultStringLength(191);
 
         RateLimiter::for('api', function (Request $request) {
