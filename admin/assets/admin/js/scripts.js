2002f7e1d902331d41ee075956f0dec858498c49
Dansday <akbaryudhanto49@gmail.com>
2026-03-26 21:39:41 +0800

    Add more time to phpfranken

--- admin/assets/admin/js/scripts.js
+++ admin/assets/admin/js/scripts.js
@@ -148,7 +148,7 @@ $(document).ready(function () {
                 if (!r.ok || ct.indexOf('application/json') === -1) {
                     return r.text().then(function (t) {
                         try { var j = JSON.parse(t); if (j.error) throw new Error(j.error); } catch (e) { if (e.message) throw e; }
-                        if (ct.indexOf('text/html') !== -1) throw new Error('Session expired. Please refresh the page and try again.');
+                        if (ct.indexOf('text/html') !== -1) throw new Error('Request timed out or server error. Please try again.');
                         throw new Error('HTTP ' + r.status + ': ' + t.substring(0, 200));
                     });
                 }
