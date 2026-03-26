e6076380df45ab115adeaf602a115292dc7e396f
Dansday <akbaryudhanto49@gmail.com>
2026-03-26 21:38:33 +0800

    Handle script error better

--- admin/assets/admin/js/scripts.js
+++ admin/assets/admin/js/scripts.js
@@ -144,9 +144,11 @@ $(document).ready(function () {
             body: JSON.stringify({ type: type, field: field, topic: promptContext })
         })
             .then(function (r) {
-                if (!r.ok) {
+                var ct = r.headers.get('content-type') || '';
+                if (!r.ok || ct.indexOf('application/json') === -1) {
                     return r.text().then(function (t) {
                         try { var j = JSON.parse(t); if (j.error) throw new Error(j.error); } catch (e) { if (e.message) throw e; }
+                        if (ct.indexOf('text/html') !== -1) throw new Error('Session expired. Please refresh the page and try again.');
                         throw new Error('HTTP ' + r.status + ': ' + t.substring(0, 200));
                     });
                 }
